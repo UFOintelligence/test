622543288dd09c3769241fdb39d7a52d5eb60038
UFOintelligence <ramonbrito10592@gmail.com>
2025-06-03 00:07:05 -0500

    Configuración inicial del paquete

--- composer.js
+++ composer.js
@@ -2,17 +2,28 @@
     "name": "ramon/mipaquete",
     "description": "Un paquete Laravel creado por Ramón",
     "type": "library",
-    "autoload": {
-        "psr-4": {
-            "Ramon\\MiPaquete\\": "src/"
+    "license": "MIT",
+    "authors": [
+        {
+            "name": "Ramón Brito",
+            "email": "ramonbrito10592@gmail.com"
         }
-    },
-    "extra": {
-        "laravel": {
-            "providers": [
-                "Ramon\\MiPaquete\\MiPaqueteServiceProvider"
-            ]
-        }
+    ],
+   "autoload": {
+    "psr-4": {
+        "MiPaquete\\": "src/"
+    }
+},
+"extra": {
+    "laravel": {
+        "providers": [
+            "MiPaquete\\MiPaqueteServiceProvider"
+        ]
+    }
+},
+    "require": {
+        "php": "^8.0",
+        "illuminate/support": "^9.0"
     },
     "minimum-stability": "dev",
     "prefer-stable": true
--- src/MiPaqueteServiceProvider.php
+++ src/MiPaqueteServiceProvider.php
@@ -1,18 +1,11 @@
 <?php
 
-namespace Ramon\MiPaquete;
+namespace MiPaquete;
 
 use Illuminate\Support\ServiceProvider;
 
 class MiPaqueteServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        // Aquí puedes hacer bindings, merge configs, etc.
-    }
-
-    public function boot()
-    {
-        // Aquí puedes cargar rutas, migraciones, vistas, publicar assets, etc.
-    }
+    public function register() {}
+    public function boot() {}
 }
